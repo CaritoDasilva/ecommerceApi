c1c9dd7a131b92c13b7bf3e669abc4adb5e6ec4a
CaritoDasilva <caritodasilva@gmail.com>
2021-02-02 19:09:05 -0300

    bla

--- eCommerceApi/urls.py
+++ eCommerceApi/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('', include('store_app.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
