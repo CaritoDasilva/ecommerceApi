7a690f0f953cf825097658c4e102b24720ba701d
CaritoDasilva <caritodasilva@gmail.com>
2021-02-03 12:42:04 -0300

    allowed cors

--- eCommerceApi/settings.py
+++ eCommerceApi/settings.py
@@ -30,6 +30,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ORIGIN_ALLOW_ALL=True
+
 
 # Application definition
 
@@ -42,9 +44,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_swagger',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
